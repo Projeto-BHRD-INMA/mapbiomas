c66928e57997afd0a19c718e49fbb4c995cd2af0
Danielle Moreira <daniomoreira@gmail.com>
2021-07-23 10:56:29 -0300

    updated scripts for Mapbiomas raster files

--- R/04_crop_and_mask_microbacias.R
+++ R/04_crop_and_mask_microbacias.R
@@ -2,24 +2,32 @@
 ## Cortar raster da bacia por microbacias
 ##
 ## Por: Danielle de Oliveira Moreira
-## Data: 18/05/2020
+## Data: 18/05/2020    update: 13/07/2021
 ################################################################
 
 
 library(raster)
 library(rgdal) #carregar shp
 
-#carregar arquivos shp
-bhrd_uph <- readOGR(dsn = "./data/albers/microbacias", layer = "bhrd_uph")
+#Load file shp
+bhrd_uph <- readOGR(dsn = "./data/shp", layer = "microbacias_wgs84")
+
+crs(bhrd_uph)
+
+#Reproject shapefile to WGS1984 (if needed)
+#bhrd_uph <- spTransform (bhrd_uph, crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+
+#writeOGR(bhrd_uph, "./data/shp", "microbacias_wgs84", driver="ESRI Shapefile", overwrite_layer = TRUE)
 
 #escolher uma microbacia
 plot(bhrd_uph)
 
 head(bhrd_uph)
+tail(bhrd_uph)
 
-bhrd_uph@data[["uph_nm"]] #para ver os nomes das subdivisoes do shp e escolher a microbacia
+bhrd_uph@data[["uph_cd"]] #para ver os nomes das subdivisoes do shp e escolher a microbacia
 
-bhrd_micro <- subset(bhrd_uph, uph_nm=="MANHUAÃ‡U")
+bhrd_micro <- subset(bhrd_uph, uph_cd=="507")
 
 plot(bhrd_uph)
 plot(bhrd_micro, col = "blue", border = FALSE, add = TRUE)
@@ -27,32 +35,42 @@ plot(bhrd_micro, col = "blue", border = FALSE, add = TRUE)
 #_------------------------CROP RASTER -----------------------------------------------
 #Carregar raster
 
-uso2018 <- raster("./data/albers/mapbiomas/mapbiomas.none.2018.tif")
+uso2019 <- raster("./data/raster/mapbiomas_col5_2019_bhrd.tif")
+uso1985 <- raster("./data/raster/mapbiomas_col5_1985_bhrd.tif")
 
-plot(uso2018)
+plot(uso1985)
 plot(bhrd_uph, border = "black", add = TRUE)
 plot(bhrd_micro, border = FALSE, col = "blue", add = TRUE)
 
 ## crop and mask
-uso2018_crop <- crop(uso2018, extent(bhrd_micro))
+uso2019_crop <- crop(uso2019, extent(bhrd_micro))
+uso1985_crop <- crop(uso1985, extent(bhrd_micro))
 
 # Após o crop
-plot(uso2018_crop)
+plot(uso2019_crop)
 plot(bhrd_uph, add = TRUE)
 
 # Adicionar uma máscara para cortar exatamente nos limites da microbacia
-uso2018_micro <- mask(uso2018_crop, bhrd_micro)
+uso2019_micro <- mask(uso2019_crop, bhrd_micro)
+uso1985_micro <- mask(uso1985_crop, bhrd_micro)
 
 ## Check that it worked
-plot(uso2018_micro)
+plot(uso1985_micro)
 plot(bhrd_uph, add=TRUE, lwd=2)
 
 # Salvar Raster cortado
 
-writeRaster(uso2018_micro, "./data/albers/mapbiomas/microbacias/mapbiomas.2018.Manhuacu.tif", driver = "ESRI Shapefile", format = 'GTiff', overwrite = T)
+writeRaster(uso2019_micro, "./data/raster/mapbiomas_col5_2019_Guandu.tif",
+            driver = "ESRI Shapefile", format = 'GTiff', overwrite = T)
 
-uso2018_micro <- raster("./data/albers/mapbiomas/microbacias/mapbiomas.2018.Manhuacu.tif")
+writeRaster(uso1985_micro, "./data/raster/mapbiomas_col5_1985_SaoJose.tif",
+            driver = "ESRI Shapefile", format = 'GTiff', overwrite = T)
 
-plot(uso2018_micro)
+
+#---------
+
+uso2019_micro <- raster("./data/raster/mapbiomas_col5_2019_SaoJose.tif")
+
+plot(uso2019_micro)
 plot(bhrd_uph, add=TRUE, lwd=2)
 
--- R/05_calculate_area_raster.R
+++ R/05_calculate_area_raster.R
@@ -1,28 +1,140 @@
 ####################################################################
-###         CALCULATE AREA FOR EACH MICRO WATERSHELTER
+###         CALCULATE LAND COVER USE AREA FOR EACH MICRO WATERSHELTER
 ###
 ### By: Danielle de Oliveira Moreira
-### date: 18/05/2020
+### date: 14/07/2020    updated:
 #################################################################
 
-library(dplyr)
 
-#Loading raster
+library(raster)
+library(rgdal) #carregar shp
 
-uso2018_micro <- raster("./data/albers/mapbiomas/microbacias/mapbiomas.2018.Manhuacu.tif")
+####Loading several rasters - mapbiomas 2019
+#uso <- raster("./data/raster/mapbiomas_col5_1985_Piracicaba.tif")
 
-plot(uso2018_micro)
+
+data <- list.files("./data/raster", pattern = "mapbiomas_col5", full.names = T)
+data
+uso <- raster(data[1])
+uso
+
+plot(uso)
+plot(bhrd_uph, add = TRUE)
 
 #calculating the area of a cell.
-##First, identify the name (in "names") of the column to group_by. In this case, "mapbiomas.2018.baixoRDoce"
-uso2018_micro
+##First, identify the name (in "names" of "uso" object) of the column to group_by.
 ##Now calculate the area
-area.uso <- as.data.frame(uso2018_micro) %>%
-    group_by(mapbiomas.2018.Manhuacu) %>%
+library(dplyr)
+area_uso <- as.data.frame(uso) %>%
+    group_by(mapbiomas_col5_1985_BaixoDoce) %>%
     tally() %>%
     mutate(area_ha = n * 0.09) #multiplying by 0.09ha or 900m2, area of each cell.
 
-area.uso
+area_uso
 
 #Save the results of frequency in txt.
-write.csv(area.uso,"./results/mapbiomas/microbacias/area-uso-2018_Manhuacu.csv", row.names = TRUE)
+write.csv(area_uso,"./results/mapbiomas_wgs84/raw/area-uso-2019_bhrd-SaoJose.csv",
+          row.names = TRUE)
+
+
+#----------------- MANIPULATING DATA -------------------#
+# Manipulating table.
+## Join tables to insert information of land cover use
+
+#Opening table with land cover use codes
+code <- read.csv2("./data/raster/legend_col5_mapbiomas.csv", encoding="utf-8")
+
+# Opening table with areas calculated
+list_table_raw <- list.files("./results/mapbiomas_wgs84/raw", pattern = ".csv", full.names = T)
+list_table_raw
+
+tab <- read.csv(list_table_raw[22], header = T, sep=",", dec=".", encoding="utf-8")
+tab
+
+
+#library(dplyr)
+colnames(tab)[2] <- "ID" #change name of column number 2
+tab2 <- dplyr::inner_join(tab,
+                          code[, c("ID", "my_class2")],
+                          by = "ID") #join tables, but only my_class column
+tab2 <- subset(tab2, select = c(my_class2, area_ha))
+
+##Changing names of table fields
+#unique(tab2_1985$my_classes)
+#tab2_2019$my_classes[tab2_2019$my_classes == "Area natural"] = "Área natural"
+#tab2_2019$my_classes[tab2_2019$my_classes == "Agua"] = "Água"
+
+##To sum areas with same classes (rows) AND add a new column (percentage)
+#library(dplyr)
+tab2019_bhrd <- tab2 %>%
+    group_by(my_class2) %>%
+    summarise(area_ha = sum(area_ha)) %>%
+    mutate(Percentage = paste0(round(area_ha/sum(area_ha)*100,2)))
+    #slice(-c(10)) %>% #to remove row with NA
+
+#Save the results of frequency in txt.
+write.csv(tab2019_bhrd,
+          "./results/mapbiomas_wgs84/modified/8_classes/area-uso-2019_bhrd3.csv",
+          row.names = TRUE)
+
+# I run each file from list_table (between lines 50 and 73, changing the object name on line 66 - to sum areas with same classes). Below are the objects created:
+tab1985_BaixoDoce
+tab1985_Caratinga
+tab1985_Guandu
+tab1985_Manhuacu
+tab1985_Piracicaba
+tab1985_Piranga
+tab1985_SantaMariaDoDoce
+tab1985_SantoAntonio
+tab1985_SaoJose
+tab1985_SuacuiGrande
+tab1985_bhrd
+
+tab2019_BaixoDoce
+tab2019_Caratinga
+tab2019_Guandu
+tab2019_Manhuacu
+tab2019_Piracicaba
+tab2019_Piranga
+tab2019_SantaMariaDoDoce
+tab2019_SantoAntonio
+tab2019_SaoJose
+tab2019_SuacuiGrande
+tab2019_bhrd
+
+
+## Or we can calculate descriptive statistics, where sum is what we want:
+#library(doBy)
+#tab4 <- summaryBy(area_ha ~ my_classes, data=tab2, FUN=c(length,sum,mean,sd))
+#tab4
+
+
+#----------------- MAKING GRAPHS -------------------#
+##Plot in a graph
+### To remove scientific format of numbers
+options(scipen = 999)
+
+library(ggplot2)
+
+## without colors
+png(file="./figs/grafico_classes1985_bhrd-Manhuacu.png", height=15, width=20, unit="cm", res=300) #to save image
+ggplot(tab2019_bhrd, aes(reorder(my_class, -area_ha, sum), area_ha, width=.7)) + #width faz a barra ficar mais fina (ou grossa)
+    geom_col() +
+    theme_classic() +
+    labs(y = "Área (ha)", x = NULL) +
+    theme (axis.text = element_text(size = 10), axis.title=element_text(size=10),
+           axis.text.x=element_text(size = 10, angle = 45, hjust = 1)) #hjust adjust text below axis x
+dev.off()
+
+
+##The palette colors
+palette <- c("#FFD966", "#1F4423", "#0000FF", "#32CD32", "#E974ED", "#935132", "#aa0000", "#FF99FF", "#af2a2a")
+
+png(file="./figs/grafico_classes_bhrd.png", height=15, width=20, unit="cm", res=300) #to save image
+ggplot(tab2019_bhrd, aes(reorder(my_class, -area_ha, sum), area_ha, fill = my_classes, width=.7)) + #width faz a barra ficar mais fina (ou grossa)
+    geom_col(fill = palette) +
+    theme_classic() +
+    labs(y = "Área (ha)", x = NULL) +
+    theme (axis.text = element_text(size = 10), axis.title=element_text(size=10),
+           axis.text.x=element_text(size = 10, angle = 45, hjust = 1)) #hjust adjust text below axis x
+dev.off()
